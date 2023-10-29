f979964a02ee007d7b7fca6f74448dc8a5c6747c
Elisabeth Villarreal <89103450+evillarreal11@users.noreply.github.com>
2023-10-29 15:41:29 -0500

    Add files via upload

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "d2945e39fd1180c44cf5acbeb93fbe0a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "db897fda066e4481bc93267bb52b8fce"
